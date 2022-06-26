17713d120b6d538d934d6ae93f6f3a9c7236ab29
ValkyrieQ8 <zahraa_amiri@hotmail.com>
2022-06-26 19:47:19 +0300

    class2

---
+++ power.cs
@@ -0,0 +1,26 @@
+﻿//Hero
+string HeroName = "Walaa";
+string HeroSuperPower = "Fish rod";
+int SPowerRate = 314;
+
+//Villain
+string VillainName = "Nemo";
+string VillainSuperPower = "Fins";
+int VPowerRate = 5;
+
+//Power
+if (SPowerRate > VPowerRate)
+{
+    Console.WriteLine("I cought a fish!");
+
+}
+else
+{
+    Console.WriteLine("I lost it!");
+}
+
+
+
+
+
+
---
+++ spped.cs
@@ -0,0 +1,20 @@
+﻿
+float PlayerSpeed = 10;
+SetSpeed(2.5f);
+
+void SetSpeed(float Sp)
+{
+    Console.WriteLine("The original speed "+ PlayerSpeed);
+    //PlayerSpeed = Sp;
+    Console.WriteLine("The new speed "+ Sp);
+
+    if (PlayerSpeed > Sp)
+    {
+        Console.WriteLine("My speed is " +PlayerSpeed);
+    }
+    else
+    {
+        Console.WriteLine("My speed is lower "+ Sp);
+    }
+}
+
